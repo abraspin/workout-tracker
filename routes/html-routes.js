167be04c167d7903bededcb73a394de0e83837c8
abraspin <abrahamspindel@gmail.com>
2020-11-17 15:45:30 -0600

    remove obs code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = function (app) {
   // All I need to do in here is set up routes for the 2 files, and then a * catch all to send me home if I want (I want)};
 
@@ -8,24 +10,30 @@ module.exports = function (app) {
     });
   });
 
-  // exercise.html
+  // // exercise.html
+  // app.get("/exercise", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  // });
+
   app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html")).catch((err) => {
-      res.status(400).json(err);
-    });
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
-  // stats.html
+  // // stats.html
+  // app.get("/stats", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/stats.html")).catch((err) => {
+  //     res.status(400).json(err);
+  //   });
+  // });
+
   app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html")).catch((err) => {
-      res.status(400).json(err);
-    });
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
-  // Redundant/catch all routes to index.html
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html")).catch((err) => {
-      res.status(400).json(err);
-    });
-  });
+  // // Redundant/catch all routes to index.html
+  // app.get("/*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/index.html")).catch((err) => {
+  //     res.status(400).json(err);
+  //   });
+  // });
 };
