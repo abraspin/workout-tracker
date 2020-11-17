f783711f1b12902cf82260b2f96f5ef3a6581da0
abraspin <abrahamspindel@gmail.com>
2020-11-17 15:48:02 -0600

    add console logs

--- public/index.js
+++ public/index.js
@@ -1,13 +1,14 @@
 init();
 
 async function init() {
+  console.log("init -> location.search.split(' = ')[1]", location.search.split("=")[1]);
   if (location.search.split("=")[1] === undefined) {
     const workout = await API.getLastWorkout();
+    console.log("init -> workout", workout);
     if (workout) {
       location.search = "?id=" + workout._id;
     } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
+      document.querySelector("#continue-btn").classList.add("d-none");
     }
   }
 }
-
